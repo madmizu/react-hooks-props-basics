a3dae8d183bb5c57736d14812a3c7974f69a4514
Madison Shimizu <mhshimizu@outlook.com>
2021-11-27 15:25:13 -1000

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+
+  // conditional rendering
+  if (!props.isPublished) {
+    return null; // will not display any DOM elements here
+  } else {
+    return (
+      <div id="blog-content">
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div> 
+    );
+  }
 }
+  // return <div id="blog-content">{props.articleText}</div>;
 
 export default BlogContent;
